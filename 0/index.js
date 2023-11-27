240696646da133b67f8f85c6b81ae2eaffb4a9f1
William Hemmingsson <william.hemmingsson@storytel.com>
2023-11-27 16:45:46 +0100

    Added example data to day 0

--- 0/index.js
+++ 0/index.js
@@ -19,3 +19,16 @@ module.exports = class Day {
     console.log(data);
   }
 };
+
+/* Example data 
+
+text,10,100.10
+19.5|10
+a string here|19.1,10,100:another string
+18.0,10,hejsan unparsable line
+191.5|10100
+0.00001|99.123
+text hejsan hoppsan,999,0.10
+unparsable,999,10
+
+*/
