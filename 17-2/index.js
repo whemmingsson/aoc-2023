5667e97596111f08f88db5bb759bab7a43b67e45
William Hemmingsson <william.hemmingsson@storytel.com>
2023-11-29 10:00:08 +0100

    Added code for 2017-2

--- 17-2/index.js
+++ 17-2/index.js
@@ -2,13 +2,39 @@ const Parser = require("../common/parser.js");
 const regex = require("../common/regexUtils.js");
 module.exports = class Day {
   static run() {
-    const data = Parser.parse(__dirname, true, [
+    const data = Parser.parse(__dirname, false, [
       {
         type: "array",
         valueType: "int",
         delimiter: " ",
       },
     ]);
-    console.log(data);
+
+    // PART ONE
+    let sum = 0;
+    data.forEach((row) => {
+      sum += Math.max(...row) - Math.min(...row);
+    });
+
+    console.log("Part 1", sum);
+
+    // PART TWO
+    const evenlyDivide = (a, b) => {
+      if (a % b === 0) return a / b;
+      if (b % a === 0) return b / a;
+      return null;
+    };
+
+    sum = 0;
+    data.forEach((row) => {
+      for (let i = 0; i < row.length; i++) {
+        for (let j = i + 1; j < row.length; j++) {
+          const v = evenlyDivide(row[i], row[j]);
+          if (v) sum += v;
+        }
+      }
+    });
+
+    console.log("Part 2", sum);
   }
 };
