b734d9427c8b3f18941e1c53a0e99002ea3b5068
William Hemmingsson <william.hemmingsson@storytel.com>
2023-12-12 12:25:23 +0100

    Cleaned up day 11

--- 11/index.js
+++ 11/index.js
@@ -13,6 +13,7 @@ module.exports = class Day {
     const grid = [];
     const galaxies = [];
 
+    /// SETUP
     let counter = 1;
     data.forEach((r, i) => {
       grid[i] = [];
@@ -28,6 +29,38 @@ module.exports = class Day {
 
     let gridHeight = grid.length;
     let gridWidth = grid[0].length;
+    /// END SETUP
+
+    const allEmptySpace = (line) => {
+      return line.every((l) => l === ".");
+    };
+
+    const drawGrid = () => {
+      console.log("GRID");
+      for (let i = 0; i < grid.length; i++) {
+        console.log(grid[i].join(""));
+      }
+    };
+
+    const getRowsToExpand = () => {
+      const rows = [];
+      for (let i = 0; i < gridHeight; i++) {
+        if (allEmptySpace(getRow(i))) {
+          rows.push(i);
+        }
+      }
+      return rows;
+    };
+
+    const getColumnsToExpand = () => {
+      const cols = [];
+      for (let i = 0; i < gridWidth; i++) {
+        if (allEmptySpace(getColumn(i))) {
+          cols.push(i);
+        }
+      }
+      return cols;
+    };
 
     const getRow = (r) => {
       if (r >= 0 && r < grid.length) return grid[r];
@@ -45,58 +78,6 @@ module.exports = class Day {
       return col;
     };
 
-    const allEmptySpace = (line) => {
-      return line.every((l) => l === ".");
-    };
-
-    const drawGrid = () => {
-      console.log("GRID");
-      for (let i = 0; i < grid.length; i++) {
-        console.log(grid[i].join(""));
-      }
-    };
-
-    const getAllRowsToDuplicate = () => {
-      const rows = [];
-      for (let i = 0; i < gridHeight; i++) {
-        if (allEmptySpace(getRow(i))) {
-          rows.push(i);
-        }
-      }
-      return rows;
-    };
-
-    const getAllColumnsToDuplicate = () => {
-      const cols = [];
-      for (let i = 0; i < gridWidth; i++) {
-        if (allEmptySpace(getColumn(i))) {
-          cols.push(i);
-        }
-      }
-      return cols;
-    };
-
-    const rowsToDuplicate = getAllRowsToDuplicate();
-    const columnsToDuplicate = getAllColumnsToDuplicate();
-
-    const duplicateRows = (rows) => {
-      for (let i = rows.length - 1; i >= 0; i--) {
-        grid.splice(rows[i], 0, new Array(gridWidth).fill("."));
-      }
-      gridHeight += rows.length;
-    };
-
-    const duplicateColumns = (cols) => {
-      for (let i = cols.length - 1; i >= 0; i--) {
-        const idxToDuplicate = cols[i];
-        for (let j = gridHeight - 1; j >= 0; j--) {
-          grid[j].splice(idxToDuplicate, 0, ".");
-        }
-      }
-
-      gridWidth += cols.length;
-    };
-
     const findGalaxies = () => {
       for (let y = 0; y < gridHeight; y++) {
         for (let x = 0; x < gridWidth; x++) {
@@ -107,7 +88,7 @@ module.exports = class Day {
       }
     };
 
-    const calculateClosest = () => {
+    const calculateDistances = () => {
       const distance = (g1, g2) => {
         return Math.abs(g2.x - g1.x) + Math.abs(g2.y - g1.y);
       };
@@ -115,23 +96,70 @@ module.exports = class Day {
       const distances = [];
 
       galaxies.forEach((galaxy, i) => {
-        //console.log("Finding closest to", galaxy.id);
         for (let j = i; j < galaxies.length; j++) {
           if (j === i) continue;
           const other = galaxies[j];
           const d = distance(galaxy, other);
           distances.push(d);
-          //console.log("  Distance from", galaxy.id, "to", other.id, "is", d);
         }
       });
 
-      console.log(distances.length);
       console.log(distances.reduce((a, b) => a + b));
     };
 
-    duplicateRows(rowsToDuplicate);
-    duplicateColumns(columnsToDuplicate);
-    findGalaxies();
-    calculateClosest();
+    const partOne = () => {
+      gridHeight = grid.length;
+      gridWidth = grid[0].length;
+
+      const rowsToDuplicate = getRowsToExpand();
+      const columnsToDuplicate = getColumnsToExpand();
+
+      const duplicateRows = (rows) => {
+        console.log("Expanding height...");
+        for (let i = rows.length - 1; i >= 0; i--) {
+          for (let n = 0; n < expansionFactor; n++) {
+            grid.splice(rows[i], 0, new Array(gridWidth).fill("."));
+          }
+        }
+        gridHeight += rows.length * expansionFactor;
+      };
+
+      const duplicateColumns = (cols) => {
+        console.log("Expanding width...");
+        for (let i = cols.length - 1; i >= 0; i--) {
+          const idxToDuplicate = cols[i];
+          for (let j = gridHeight - 1; j >= 0; j--) {
+            for (let n = 0; n < expansionFactor; n++) {
+              grid[j].splice(idxToDuplicate, 0, ".");
+            }
+          }
+        }
+
+        gridWidth += cols.length * expansionFactor;
+      };
+
+      duplicateRows(rowsToDuplicate);
+      duplicateColumns(columnsToDuplicate);
+      findGalaxies();
+      calculateDistances();
+    };
+
+    const partTwo = () => {
+      findGalaxies();
+      const f = 999999;
+
+      const expand = (s, a) => {
+        s.forEach((c, i) => {
+          galaxies.filter((g) => g[a] > c + 999999 * i).forEach((g) => (g[a] += f));
+        });
+      };
+
+      expand(getRowsToExpand(), "y");
+      expand(getColumnsToExpand(), "x");
+      calculateDistances();
+    };
+
+    //partOne();
+    partTwo();
   }
 };
