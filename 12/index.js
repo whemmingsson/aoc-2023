e791dd70463ae31650503f74f59b4870ecda8ed2
William Hemmingsson <william.hemmingsson@storytel.com>
2023-12-12 17:39:17 +0100

    Day 12 part 1

--- 12/index.js
+++ 12/index.js
@@ -1,7 +1,9 @@
+const Parser = require("../common/parser.js");
 module.exports = class Day {
   static run() {
-    const row = "?###???????? 3,2,1";
-    const validMap = ".###.##.#...";
+    const data = Parser.readRaw(__dirname, true).split("\n");
+
+    console.log(data);
 
     const parseMap = (r) => {
       return r;
@@ -21,21 +23,49 @@ module.exports = class Day {
       return { map, groups };
     };
 
-    const validArrangement = (r) => {
-      if (r.map.indexOf("?") >= 0) return false;
+    const isValid = (map, groups) => {
+      if (map.indexOf("?") >= 0) return false;
 
-      const mapGroups = r.map
+      const mapGroups = map
         .split(".")
         .filter((v) => v !== "")
         .map((v) => v.length);
 
-      if (mapGroups.length !== r.groups.length) return false;
+      if (mapGroups.length !== groups.length) return false;
 
       for (let i = 0; i < mapGroups.length; i++) {
-        if (mapGroups[i] !== r.groups[i]) return false;
+        if (mapGroups[i] !== groups[i]) return false;
       }
 
       return true;
+    };
+
+    const options = ["#", "."];
+    const generatePermutations = (options, length) => {
+      if (length === 0) return [];
+
+      const right = generatePermutations(options, length - 1);
+
+      if (right.length == 0) {
+        return options;
+      }
+
+      const result = [];
+      options.forEach((opt) => {
+        right.forEach((r) => {
+          result.push(opt + r);
+        });
+      });
+
+      return result;
+    };
+
+    const generatePossibleMap = (per, map, unknownIndecies) => {
+      let copy = map.split("");
+      unknownIndecies.forEach((p, i) => {
+        copy[p] = per[i];
+      });
+      return copy.join("");
     };
 
     const countValidArrangements = (r) => {
@@ -44,20 +74,24 @@ module.exports = class Day {
       for (let i = 0; i < r.map.length; i++) {
         if (r.map[i] === "?") unknownIndecies.push(i);
       }
-      console.log(unknownIndecies);
-      // Validate all arrangement (count # of valid ones)
-      // Return count
+
+      let validCount = 0;
+      generatePermutations(options, unknownIndecies.length).forEach((per) => {
+        const map = generatePossibleMap(per, r.map, unknownIndecies);
+        if (isValid(map, r.groups)) {
+          validCount++;
+        }
+      });
+
+      return validCount;
     };
 
-    // Test functionality
-    const r = parseRow(row);
-    //console.log(validArrangement(r));
+    let sum = 0;
+    data.forEach((row) => {
+      const r = parseRow(row);
+      sum += countValidArrangements(r);
+    });
 
-    countValidArrangements(r);
-
-    // Lab
-    const options = ["A", "B"];
-    const str = "x|x";
-    const indicies = [0, 2];
+    console.log("Part 1", sum);
   }
 };
