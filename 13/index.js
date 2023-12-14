28a30216450b27e3037fb9a8e4ceff28a982a48a
William Hemmingsson <william.hemmingsson@storytel.com>
2023-12-14 16:08:13 +0100

    Day 13 part 2

--- 13/index.js
+++ 13/index.js
@@ -37,12 +37,7 @@ module.exports = class Day {
       let left = getColumn(m, i);
       let right = getColumn(m, j);
 
-      console.log("  Starting finding vertical reflection");
-
       while (right == left && right && left) {
-        console.log("    --");
-        console.log("    LEFT: ", left);
-        console.log("    RIGHT:", right);
         i--;
         j++;
         left = getColumn(m, i);
@@ -58,29 +53,21 @@ module.exports = class Day {
       let top = getRow(m, i);
       let bottom = getRow(m, j);
 
-      console.log("  Starting finding horizontal reflection", top, bottom);
-
       while (top == bottom && i >= 0 && j <= m.length - 1) {
-        console.log("    --");
-        console.log("    TOP: ", top);
-        console.log("    BTM:", bottom);
         i--;
         j++;
         top = getRow(m, i);
         bottom = getRow(m, j);
       }
 
-      console.log("    *--");
-      console.log("    TOP: ", top);
-      console.log("    BTM:", bottom);
-
       if (top == bottom || top == "" || bottom == "") {
         return true;
       }
     };
     let sum = 0;
-    maps.forEach((m, n) => {
-      console.log("Inspecting map", n);
+    let start = new Date();
+
+    const findMirror = (m, prev) => {
       // Find using columns
       for (let i = 0; i < m[0].length - 1; i++) {
         let currentCol = getColumn(m, i);
@@ -88,8 +75,12 @@ module.exports = class Day {
         if (currentCol == nextCol) {
           const isMirror = isVerticalMirror(m, i, i + 1);
           if (isMirror) {
-            console.log(" MIRROR FOUND - Columns to the left:", m[0].length - i);
-            sum += m[0].length - i;
+            //sum += i + 1;
+            let k = "v_" + i;
+            if (prev && k !== prev) {
+              return k;
+            }
+            if (!prev) return k;
           }
         }
       }
@@ -101,14 +92,62 @@ module.exports = class Day {
         if (currentRow == nextRow) {
           const isMirror = isHorizontalMirror(m, i, i + 1);
           if (isMirror) {
-            console.log("MIRROR FOUND - Rows above x 100", (m.length - i) * 100);
-            sum += (m.length - i) * 100;
+            //sum += (i + 1) * 100;
+            let k = "h_" + i;
+            if (prev && k !== prev) {
+              return k;
+            }
+            if (!prev) return k;
           }
         }
       }
+      return "";
+    };
 
-      console.log("----------------------");
+    String.prototype.replaceAt = function (index, replacement) {
+      return this.substring(0, index) + replacement + this.substring(index + replacement.length);
+    };
+
+    sum = 0;
+    maps.forEach((m, n) => {
+      let mirror = null;
+      console.log("Starting map", n);
+      mirror = findMirror(m);
+      console.log("Original mirror:", mirror);
+      console.log(m);
+      let foundNew = false;
+      for (let a = 0; a < m.length; a++) {
+        for (let b = 0; b < m[a].length; b++) {
+          // Switch character
+
+          if (m[a][b] === ".") m[a] = m[a].replaceAt(b, "#");
+          else if (m[a][b] === "#") m[a] = m[a].replaceAt(b, ".");
+
+          let newMirror = findMirror(m, mirror);
+          if (newMirror) {
+            console.log("Generated new mirror!", mirror, "-->", newMirror);
+            console.log(m);
+            const split = newMirror.split("_");
+            const v = parseInt(split[1]);
+            if (split[0] === "v") {
+              sum += v + 1;
+            } else {
+              sum += (v + 1) * 100;
+            }
+            foundNew = true;
+            break;
+          }
+          // Switch back character
+          if (m[a][b] === ".") m[a] = m[a].replaceAt(b, "#");
+          else if (m[a][b] === "#") m[a] = m[a].replaceAt(b, ".");
+        }
+        if (foundNew) {
+          break;
+        }
+      }
     });
+
+    console.log(new Date() - start);
 
     console.log(sum);
   }
